4494e4d8dcc744db66fb7b704aca40b9b2057fea
Santiago840 <santyemma32@gmail.com>
2025-02-13 15:26:22 -0600

    Usuario Requests validaciones

--- tienda-app/app/Http/Controllers/UserController.php
+++ tienda-app/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -34,21 +35,9 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(UserRequest $request): RedirectResponse
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|min:8|max:255',
-            'rol' => 'required|string'
-        ], [
-
-            'name.required' => 'El nombre es obligatorio',
-            'email.unique' => 'Este correo ya existe',
-            'password.min' => 'La contraseña debe tener al menos :min caracteres'
-        ]);
-
-        User::create($validated);
+        User::create($request->validated());
 
         return redirect(route('usuarios.index'))->with('success', 'Usuario creado exitosamente.');
     }
@@ -72,17 +61,11 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user): RedirectResponse
+    public function update(UserRequest $request, User $user): RedirectResponse
     {
         Gate::authorize('updateUsuario', $user);
 
-        $validate = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|max:255|unique:users,email',
-            'rol' => 'required|string'
-        ]);
-
-        $user->update($validate);
+        $user->update($request->validated());
 
         return redirect(route('usuarios.index'))->with('success', 'Usuario correctamente actualizado.');
     }
